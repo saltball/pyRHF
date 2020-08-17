54f8c33856d70b81cfcc861fcc248f10ce4c4ac5
hanyanbo <yanbohan98@gmail.com>
2020-08-17 20:13:56 +0800

    20200817

--- QuanChemComp/core/Molecule/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# ====================================== #
-# @Author  : Yanbo Han
-# @Email   : yanbohan98@gmail.com
-# @File    : __init__.py.py
-# ALL RIGHTS ARE RESERVED UNLESS STATED.
-# ====================================== #
-
